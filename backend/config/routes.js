b09545814383f35725810a5925e9e2840838cad7
George Albuquerque <georgealbuquerquepinto@hotmail.com>
2017-06-27 20:48:01 -0300

    Criando o Arquivo de Rotas

---
+++ backend/config/routes.js
@@ -0,0 +1,7 @@
+const express = require('express')
+
+module.express = function(server) {
+  // API Routes
+  const routes = express.Routes()
+  server.use('/api', router)
+}
--- backend/config/server.js
+++ backend/config/server.js
@@ -11,11 +11,4 @@ server.listen(port, function() {
   console.log(`BACKEND is running on port ${port}.`)
 })
 
-server.use(function(req, res, next){
-  console.log('meu middleware 1')
-  next()
-})
-
-server.use(function(req, res, next){
-  console.log('meu middleware 2')
-})
+module.exports = server
