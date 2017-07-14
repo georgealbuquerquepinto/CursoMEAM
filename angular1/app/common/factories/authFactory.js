ace539f23f67d0e97a50470aa6dd2b2a58e03edf
George Albuquerque <georgealbuquerquepinto@hotmail.com>
2017-07-14 16:15:54 -0300

    Update

---
+++ angular1/app/common/factories/authFactory.js
@@ -0,0 +1,62 @@
+(function () {
+  angular.module('primeiraApp').factory('auth', [
+    '$http',
+    'consts',
+    AuthFactory
+  ])
+
+  function AuthFactory($http, consts) {
+    let user = null
+
+    function getUser() {
+      if (!user) {
+          user = JSON.parse(localStorage.getItem(consts.userKey))
+      }
+      return user
+    }
+
+    function signup(user, callback) {
+      submit('signup', user, callback)
+    }
+
+    function login(user, callback) {
+      submit('login', user, callback)
+    }
+
+    function submit(url, user, callback) {
+      $http.post(`${consts.oapiUrl}/${url}`, user).then(resp => {
+        localStorage.setItem(consts.userKey, JSON.stringify(response.data))
+        $http.defaults.headers.common.Authorization = response.data.token
+        if (callback) callback(null, response.data)
+      }).catch(function (resp) {
+        if (callback) callback(response.data.errors, null)
+      })
+    }
+
+    function logout(callback) {
+      user = null
+      localStorage.removeItem(consts.userKey)
+      $http.defaults.headers.common.Authorization = ''
+      if (callback) callback(null)
+    }
+
+    function validateToken(token, callback) {
+      if (token) {
+        $http.post(`${consts.oapiUrl}/validateToken`, { token }).then(resp => {
+          if (!response.data.valid) {
+            logout()
+          } else {
+            $http.defaults.headers.common.Authorization = getUser().token
+          }
+          if (callback) callback(null, response.data.valid)
+        }).catch(function (resp) {
+          if (callback) callback(response.data.errors)
+        })
+      } else {
+        if (callback) callback('Token inválido.')
+      }
+    }
+
+    return { signup, login, logout, getUser, validateToken }
+  }
+})()
